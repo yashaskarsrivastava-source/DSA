4dd5acd815f9061d3c865ac088453afc8726bbfa
yashaskarsrivastava-source <yashaskarsrivastava@gmail.com>
2026-07-23 00:53:41 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (14.51%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x == 0) return 0;
+        
+        long long left = 1, right = x;
+        long long ans = 0;
+        
+        while (left <= right) {
+            long long mid = left + (right - left) / 2;
+            
+            if (mid * mid <= x) {
+                ans = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        
+        return (int)ans;
+    }
+};
